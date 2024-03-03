549adc0a708da16e3422b8bbb4c3cf710b0e6590
Gilson Reis <gilson.reis@epoc.tech>
2024-03-03 18:37:04 -0300

    List and filter users

--- app/Livewire/Users/Index.php
+++ app/Livewire/Users/Index.php
@@ -2,13 +2,25 @@
 
 namespace App\Livewire\Users;
 
+use App\Models\User;
+use App\UseCases\Users\ListAllUsersUseCase;
 use Livewire\Attributes\Title;
+use Livewire\Attributes\Url;
 use Livewire\Component;
+use Livewire\WithPagination;
+
 #[Title('List Users')]
 class Index extends Component
 {
-    public function render()
+    use WithPagination;
+
+    #[Url(keep: true)]
+    public ?string $search = null;
+
+    public function render(ListAllUsersUseCase $listAllUsersUseCase)
     {
-        return view('livewire.users.index');
+        return view('livewire.users.index', [
+            'users' => $listAllUsersUseCase->handle($this->search)
+        ]);
     }
 }
